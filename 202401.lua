887e54d04643d2869e819e36dd8a1ecacde09083
Böckler, Kevin <Kevin.Boeckler@mach.de>
2025-11-24 15:29:13 +0100

    Extract functions

--- 202401.lua
+++ 202401.lua
@@ -1,3 +1,23 @@
+local function distanceTo(leftNumber, i, rightNumbers)
+	local rightNum = rightNumbers[i]
+	local distance = rightNum - leftNumber
+	if (distance < 0) then
+		distance = -1 * distance
+	end
+	return distance
+end
+
+local function similarityTo(leftNumber, rightNumbers)
+	local amountRight = 0
+	for _, rn in ipairs(rightNumbers) do
+		if rn == leftNumber then
+			amountRight = amountRight + 1
+		end
+	end
+	local similarity = (leftNumber * amountRight)
+	return similarity
+end
+
 local input = require("aoc").input(2024, 1)
 local left = {}
 local right = {}
@@ -17,19 +37,8 @@ table.sort(right)
 local sumDistance = 0
 local sumSimilarity = 0
 for i, leftNum in ipairs(left) do
-	local rightNum = right[i]
-	local distance = rightNum - leftNum
-	if (distance < 0) then
-		distance = -1 * distance
-	end
-	sumDistance = sumDistance + distance
-	local amountRight = 0
-	for _, rn in ipairs(right) do
-		if rn == leftNum then
-			amountRight = amountRight + 1
-		end
-	end
-	sumSimilarity = sumSimilarity + (leftNum * amountRight)
+	sumDistance = sumDistance + distanceTo(leftNum, i, right)
+	sumSimilarity = sumSimilarity + similarityTo(leftNum, right)
 end
 print("sumDistance: " .. sumDistance)
 print("sumSimilarity: " .. sumSimilarity)

1dcfa38058987b5f7eb75076a9b243ace20f529c
Kevin Böckler <kevinboeckler@gmail.com>
2025-12-03 20:05:01 +0100

    Finish day 1

--- 202501.lua
+++ 202501.lua
@@ -1,4 +1,5 @@
 local input = require("aoc").input(2025, 1)
+-- parsing
 local changes = {}
 for row in string.gmatch(input, "[^\n]+") do
 	local leading = string.sub(row, 1, 1)
@@ -17,12 +18,26 @@ for row in string.gmatch(input, "[^\n]+") do
 
 	table.insert(changes, change)
 end
+-- calculating
 local countZeroDials = 0
-local dialValue = 50 + 10000
+local clicks = 0
+local dialValue = 50 + 100000
 for _, change in ipairs(changes) do
-	dialValue = dialValue + change
+	local oldDialValue = dialValue
+	local newDialValue = dialValue + change
+	dialValue = newDialValue
 	if math.fmod(dialValue, 100) == 0 then
 		countZeroDials = countZeroDials + 1
 	end
+	local oldClickFits = math.floor(oldDialValue / 100)
+	local newClickFits = math.floor(newDialValue / 100)
+	clicks = clicks + math.abs(newClickFits - oldClickFits)
+	if change < 0 and math.fmod(dialValue, 100) == 0 then
+		clicks = clicks + 1
+	end
+	if change < 0 and math.fmod(oldDialValue, 100) == 0 then
+		clicks = clicks - 1
+	end
 end
 print("countZeroDials: " .. countZeroDials)
+print("clicks: " .. clicks)

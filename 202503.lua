58ca30ce30cdb5db2e57c50f5ff7fb62bf4c9704
Kevin Böckler <kevinboeckler@gmail.com>
2025-12-05 07:30:23 +0100

    Day3 Finish

--- 202503.lua
+++ 202503.lua
@@ -1,23 +1,32 @@
-local input = require("aoc").input(2025, 3)
-local totalJoltage = 0
-for row in string.gmatch(input, "[^\n]+") do
-	local maxFirst = 0
-	local maxFirstIndex = -1
-	for i = 1, string.len(row) - 1 do
-		local num = tonumber(row:sub(i, i))
-		if num > maxFirst then
-			maxFirst = num
-			maxFirstIndex = i
+local function calcJoltageOfBank(bank, batteryAmount)
+	local maxNumbers = {}
+	local maxNumberIndices = {}
+	for _ = 1, batteryAmount do
+		table.insert(maxNumbers, 0)
+		table.insert(maxNumberIndices, -1)
+	end
+	for currentNumber = 1, batteryAmount do
+		local searchStart = 1
+		if currentNumber > 1 then
+			searchStart = maxNumberIndices[currentNumber - 1] + 1
+		end
+		local searchEnd = string.len(bank) - (batteryAmount - currentNumber)
+		for i = searchStart, searchEnd do
+			local num = tonumber(bank:sub(i, i)) or 0
+			if num > maxNumbers[currentNumber] then
+				maxNumbers[currentNumber] = num
+				maxNumberIndices[currentNumber] = i
+			end
 		end
 	end
-	local maxSecond = 0
-	for i = maxFirstIndex + 1, #row do
-		local num = tonumber(row:sub(i, i))
-		if num > maxSecond then
-			maxSecond = num
-		end
-	end
-	local joltage = tonumber(maxFirst .. maxSecond)
-	totalJoltage = totalJoltage + joltage
+	return tonumber(table.concat(maxNumbers)) or 0
 end
-print("totalJoltage: " .. totalJoltage)
+local input = require("aoc").input(2025, 3)
+local totalJoltage2 = 0
+local totalJoltage12 = 0
+for row in string.gmatch(input, "[^\n]+") do
+	totalJoltage2 = totalJoltage2 + calcJoltageOfBank(row, 2)
+	totalJoltage12 = totalJoltage12 + calcJoltageOfBank(row, 12)
+end
+print("totalJoltage2: " .. totalJoltage2)
+print("totalJoltage12: " .. totalJoltage12)
